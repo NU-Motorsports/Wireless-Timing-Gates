fbcc0910bc866746eefae5bc322490e69456d8dd
djw319 <64276595+djw319@users.noreply.github.com>
2022-11-21 15:50:20 -0500

    Timer Testing First prototype

--- Timer_test/Timer_test/Timer_test.ino
+++ Timer_test/Timer_test/Timer_test.ino
@@ -1,14 +1,41 @@
 const int button_pin = 11;
+const int led_pin = 13;
+bool gatestate = 0;
+float timervar = 0;
+float stopwatch = 0;
 
 
 void setup() {
   // put your setup code here, to run once:
   pinMode(button_pin,INPUT);
+  pinMode(led_pin,OUTPUT);
   Serial.begin(9600);
+  digitalWrite(led_pin,LOW);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if buttonpin
-
+  gatestate = digitalRead(button_pin);
+  
+  if (gatestate == HIGH)  {
+    gatestate = LOW;
+    timervar = millis();
+    digitalWrite(led_pin,HIGH);
+    Serial.println("Timer Start");
+    delay(100);
+    
+    
+    do {
+      gatestate = digitalRead(button_pin);
+      delay(100);
+      
+    } while(gatestate == LOW);
+    stopwatch = millis() - timervar;
+    Serial.print("Timer stopped: ");
+    Serial.print(stopwatch);
+    Serial.println(" seconds");
+    digitalWrite(led_pin,LOW);
+    delay(100);
+    
+  }
 }

438dbf863e6a69a300ffb720631185500eb47514
djw319 <64276595+djw319@users.noreply.github.com>
2023-01-06 18:39:43 -0600

    Functional Timer Test Code

--- Timer_test/Timer_test/Timer_test.ino
+++ Timer_test/Timer_test/Timer_test.ino
@@ -1,11 +1,11 @@
 #include "esp_timer.h"
 
-const int button_pin = 15;
-const int led_pin = 13;
+const int button_pin = 4;
+const int led_pin = 2;
 bool gateState = 0;
 bool lastGateState = 0;
 float timervar = 0;
-int debounceDelay = 4000; //in microseconds
+int debounceDelay = 2000; //in microseconds
 float stopwatch = 0;
 bool wheelnum = 0;
 int lastDebounceTime = 0;
@@ -15,42 +15,47 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(button_pin,INPUT);
   pinMode(led_pin,OUTPUT);
-  Serial.begin(115200);
+  Serial.begin(9600);
   digitalWrite(led_pin,LOW);
   Serial.write("Serial Test");
 }
 
 void loop() {
   bool gateReading = digitalRead(button_pin);
+//  Serial.print(esp_timer_get_time());
+//  Serial.print("   ");
+//  Serial.print(esp_timer_get_time()-lastDebounceTime);
+//  Serial.print("   ");
+//  Serial.println(gateReading);
 
   //Detect start of a press
-  if (gateReading != lastGateState){
+  if(gateReading != lastGateState){
     lastDebounceTime = esp_timer_get_time();
   }
 
   //Continue if press remains longer then the debounce delay
   if((esp_timer_get_time()-lastDebounceTime)>debounceDelay){
-    if(gateReading != gateState){                   //
+    if(gateReading != gateState){
       gateState = gateReading;
       if(gateState == HIGH){
         if(wheelnum == 0){
-          timervar = esp_timer_get_time()/1000000.00;
+          timervar = esp_timer_get_time();
           digitalWrite(led_pin,HIGH);
-          Serial.println("Timer Start ");
+          //Serial.println("Timer Start ");
           wheelnum = 1;
         }else if(wheelnum == 1){
-          stopwatch = esp_timer_get_time()/1000000.000 - timervar;
+          stopwatch = (esp_timer_get_time() - timervar)/1000000.000;
           digitalWrite(led_pin,LOW);
           Serial.print("Timer stopped: ");
           Serial.print(stopwatch);
-          Serial.print(" seconds");
+          Serial.println(" seconds");
           wheelnum = 0;
         }
       }
     }
   }
 
-  lastGateState = gateState;
+  lastGateState = gateReading;
 }
 
 //functions

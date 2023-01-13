f4d3e03ed163f1e1290da2c4221c915d34000724
djw319 <64276595+djw319@users.noreply.github.com>
2023-01-13 00:51:37 -0500

    Work on Timing Gate Receiver

--- Timing_Gate_Receiver/Timing_Gate_Receiver.ino
+++ Timing_Gate_Receiver/Timing_Gate_Receiver.ino
@@ -7,7 +7,8 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <WiFi.h>
-#include<esp_now.h>
+#include <esp_now.h>
+#include <esp_timer.h>
 
 
 //Screen Setup
@@ -18,20 +19,40 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 //Inupt Pins
-const int modePin = 23;
+const int page_pin = 23;
 const int led_pin = 18;
 
 //Speed Calc Variables
 float wheelbase = 55.0;       //inches
-float speedValue =0;          //mph
+float speedValue = 0;          //mph
 
 //Received Data Variables
+bool messageReceivedStatus = 0;
 int gateReceived = 0;
 int speedTimeReceived = 0;
 bool speedStatusReceived = 0;
 
+//Lap Timing Variables
+int lapStartTime = 0;
+float lastLapDuration = 0;
+float fastestLapDuration = 0;
 
+//Button Debounce Variables
 bool buttonstate = 0;
+int buttonDebounceDelay = 16000;
+int lastButtonDebounceTime = 0;
+bool pageReading = 0;
+bool lastPageState = 0;
+bool pageState = 0;
+
+//Page Variables
+int pageStatus = 0;
+//1: Recent Entry Overview
+//2: All Recent Sectors
+//3: Fastest Lap and Sectors
+//4: Top Speed
+//5: Broadcast Address
+int num_pages = 5;
 
 
 //Message Structure
@@ -44,59 +65,86 @@ typedef struct struct_message {
 
 struct_message myData;
 
-//Sync Structure
 
+//Data Received Callback function
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {  
   memcpy(&myData, incomingData, sizeof(myData));
 
-  gateReveived = myData.a;
+  gateReceived = myData.a;
   speedTimeReceived = myData.c;
   speedStatusReceived = myData.d;
 
-  speedValue = (wheelbase/speedTimeReceived)/56820;       //Speed value in mph
+  messageReceivedStatus = 1;
+
+  Serial.print("Gate: ");
+  Serial.print(myData.a);
+  Serial.print("     Speed Time: ");
+  Serial.println(speedTimeReceived);
   
-  
-  Serial.print(myData.a);     //Gate Number
-  Serial.print("     ");
-  Serial.print(myData.b);     //Triggered?
-  Serial.print("     ");
-  Serial.print(myData.c);   //Speed (mph, will return 5 if less than 5mph
-  Serial.print("     ");
-  Serial.print(myData.d);   //Speed (mph, will return 5 if less than 5mph
-  Serial.print("    Speed: ");
-  Serial.println(speedValue);
-  
-  if(myData.b == 1){
-    digitalWrite(led_pin, HIGH);
-  } else  {
-    digitalWrite(led_pin, LOW);
-  }
 }
 
 void setup() {
-  pinMode(modePin, INPUT);
+  pinMode(page_pin, INPUT);
   pinMode(led_pin, OUTPUT);
   WiFi.mode(WIFI_MODE_STA);
   Serial.begin(9600);
-
   initScreen();
-  
   if  (esp_now_init() != ESP_OK)  {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
-  
   esp_now_register_recv_cb(OnDataRecv);
 }
 
 void loop() {
+  inputread();
 
+    speedValue = (wheelbase/speedTimeReceived)/56820;       //Speed value in mph
+  
 
+  //On Data Received Actions
+  if(messageReceivedStatus == 1){
+    if(gateReceived == 0){
+      lapStartTime = esp_timer_get_time();
+    }else if(gateReceived == 8){
+      lastLapDuration = (esp_timer_get_time() - lapStartTime)/1000000.0;
+    }else if(gateReceived == 9){
+      lastLapDuration = (esp_timer_get_time() - lapStartTime)/1000000.0;
+      lapStartTime = esp_timer_get_time();
+    }
+    messageReceivedStatus = 0;
+    Serial.println(lastLapDuration);
+    updateDisplay();
+  }
+  
+
+  
+  //Page Triggered
+  if(pageReading != lastPageState){
+    lastButtonDebounceTime = esp_timer_get_time();
+  }
+  
+  if((esp_timer_get_time()-lastButtonDebounceTime)>buttonDebounceDelay){
+    if(pageReading != pageState){
+      pageState = pageReading;
+      if(pageState == HIGH){
+        if(pageStatus<(num_pages-1)){
+          pageStatus = pageStatus+1;
+          updateDisplay();
+        }else{
+          pageStatus = 0;
+          updateDisplay();
+        }
+      }
+    }
   }
 
+  lastPageState = pageReading;
+}
 
 
 
+//////////////////////////////////FUNCTION//////////////////////////////////////
 void initScreen(){
   
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
@@ -112,5 +160,37 @@ void initScreen(){
   display.setCursor(13,9);
   display.println(WiFi.macAddress());
   display.display();
-  delay(2000);
+}
+
+
+//Screen Update
+void updateDisplay(){
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  if(pageStatus == 0){
+    updateDisplay_Info();
+  }else{
+    display.setCursor(25,0);
+    display.println("NU Motorsports");
+    display.setCursor(0,18);
+    display.print("Empty Mode Screen");
+  }
+  display.display();
+}
+
+
+//Screen Update Information
+void updateDisplay_Info(){
+  display.setCursor(0,18);
+  display.print("Recent Information: ");
+  display.setCursor(0,36);
+  display.print("Last Lap: ");
+  display.print(lastLapDuration);
+}
+
+
+//Input Read
+void inputread(){
+  pageReading = digitalRead(page_pin);
 }
